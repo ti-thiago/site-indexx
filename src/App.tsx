f8099d040cd7bc3b27988a9720e78a75b9e59bba
ti-thiago <54894891+ti-thiago@users.noreply.github.com>
2021-05-01 08:57:50 -0300

    commit site-index(falta trocar o favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import logo from "./asteroth-logo.png";
+import tibiaLogo from "./tibia-logo.png";
+import lineageLogo from "./lineage-logo.png";
+import "./App.css";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="content">
+        <div className="logo">
+          <p>Bem vindo a</p>
+          <img src={logo} alt="" />
+        </div>
+
+        <div className="games">
+          <div className="lineage">
+            <img src={lineageLogo} alt="Lineage2 Logo" />
+            <h2>L2 Essence Server</h2>
+            <p>Jogue Lineage2 com a nova classe, Death Knight!</p>
+            <button
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                window.open("http://lineage.asteroth.com.br", "_blank");
+              }}
+              className="button button--mimas"
+            >
+              <span>Play Lineage 2 Essence</span>
+            </button>
+          </div>
+
+          <div className="tibia">
+            <img src={tibiaLogo} alt="Tibia Logo" />
+            <h2>Open Tibia Server</h2>
+            <p>Conquiste e domine a ilha de Asteroth em Tibia</p>
+            <button
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                window.open("http://tibia.asteroth.com.br", "_blank");
+              }}
+              className="button button--mimas"
+            >
+              <span>Play Tibia 12.60</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
